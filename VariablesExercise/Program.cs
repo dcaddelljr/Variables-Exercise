0b3fa07e905e745f7380013e45fe7b1b87381906
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-03 08:15:09 -0600

    new VabEx

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int myDogs = 2;
+
+            string myFullName = "Dwight" + " " + "Caddell";
+            char middleInitial = 'A';
+            bool isConfused = true;
+
+            double gradYear = 1998;
+
+            //Decimal
+            var yearsTaught = 12.5m;
+
+            Console.WriteLine($"My name is {myFullName} with a middle inital of {middleInitial}.");
+            Console.WriteLine($"I have {myDogs} dogs.");
+            Console.WriteLine($"I graduated high school in {gradYear} and taught school for {yearsTaught}.");
+            Console.WriteLine($"{isConfused}");
+
         }
     }
 }
